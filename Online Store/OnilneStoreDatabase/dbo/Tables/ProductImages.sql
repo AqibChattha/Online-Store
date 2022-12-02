97b7991e1a863b6d1df7918b637d194e551c9950
AqibChattha <aqibali543@gmail.com>
2022-12-03 04:29:08 +0500

    add - SQL Server Database project in c#

---
+++ Online Store/OnilneStoreDatabase/dbo/Tables/ProductImages.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ProductImages] (
+    [id]            INT             IDENTITY (1, 1) NOT NULL,
+    [productId]     INT             NOT NULL,
+    [image]         VARBINARY (MAX) NOT NULL,
+    [previewImage1] VARBINARY (MAX) NULL,
+    [previewImage2] VARBINARY (MAX) NULL,
+    [previewImage3] VARBINARY (MAX) NULL,
+    CONSTRAINT [PK_ProductImages] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_ProductImages_Products] FOREIGN KEY ([productId]) REFERENCES [dbo].[Products] ([id])
+);
+
---
+++ Online Store/OnilneStoreDatabase/dbo/Tables/Products.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [id]            INT             IDENTITY (1, 1) NOT NULL,
+    [description]   NVARCHAR (500)  NULL,
+    [model/brand]   NVARCHAR (50)   NULL,
+    [stockQuantity] INT             NULL,
+    [soldQuantity]  INT             NULL,
+    [price]         DECIMAL (18, 2) NOT NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
